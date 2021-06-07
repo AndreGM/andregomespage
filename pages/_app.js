151e84e227f091d5dc9945b92dd2ab32cf2d411e
andregm <andre@appdev.com.br>
2021-06-07 11:59:14 -0300

    added tagmanager

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
+import React, {useEffect } from 'react';
 import '../styles/globals.css'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  id: 'GTM-W2MWDBS'
+}
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
   return <Component {...pageProps} />
 }
 
