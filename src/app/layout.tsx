5d1cda5767d87d62301c9e4f7434a6f83b98e96a
Andre Gomes <andre@appdev.com.br>
2023-07-07 21:06:28 -0300

    new next version

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Andre Gomes Page',
+  description: 'Page under construction 🚧',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
